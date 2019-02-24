0bbefec868e93abc79a76cdf6dd34ab9207fa4ac
Kasandra Wolf <wolf.kasandra@gmail.com>
2019-02-23 18:09:51 -0700

    pass id through props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      }, {
+        id: 2,
+        title: 'Dinner with bae',
+        completed: false
+      }, {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: true
+      }
+    ]
+  }
+
+  markComplete = (id) => {
+    console.log('id: ' + id)
+  }
   render() {
+    // console.log(this.state.todos)
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ?
+        'line-through' : 'none'
+    }
+  }
+  // markComplete = (event) => {
+
+  // }
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      // <div style={{ backgroundColor: '#f4f4f4' }}>
+      // <div style={itemStyle}>
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+          {' '}
+          {title}</p>
+      </div>
+    )
+  }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+// const itemStyle = {
+//   backgroundColor: '#f4f4f4'
+// }
+
+export default TodoItem
